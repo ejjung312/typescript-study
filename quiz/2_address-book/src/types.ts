dee00e95049080e6e2ae066b94de1379f0604262
jungeunji <ejjung312@gmail.com>
2022-06-08 20:12:20 +0900

    타입 모듈화

---
+++ quiz/2_address-book/src/types.ts
@@ -0,0 +1,22 @@
+interface PhoneNumberDictionary {
+  [key: string]: {
+    num: number;
+  };
+}
+
+interface Contact {
+  name: string;
+  address: string;
+  phones: PhoneNumberDictionary;
+}
+
+enum PhoneType {
+  Home = 'home',
+  Office = 'office',
+  Studio = 'studio'
+}
+
+export {
+    Contact,
+    PhoneType
+}
