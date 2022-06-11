368efbb984c312c0a674de458114ef0b094c433b
jungeunji <ejjung312@gmail.com>
2022-06-11 12:29:58 +0900

    axios, chart.js 외부 라이브러리 설치

--- project/src/app.ts
+++ project/src/app.ts
@@ -1,3 +1,6 @@
+import axios from 'axios';
+import { Chart } from 'chart.js';
+
 // utils
 function $(selector: string) {
   return document.querySelector(selector);
@@ -177,10 +180,10 @@ async function setupData() {
 
 function renderChart(data: any, labels: any) {
   const ctx = $('#lineChart').getContext('2d');
-  // Chart.defaults.color = '#f5eaea';
-  // Chart.defaults.font.family = 'Exo 2';
-  Chart.defaults.global.defaultFontColor = '#f5eaea';
-  Chart.defaults.global.defaultFontFamily = 'Exo 2';
+  Chart.defaults.color = '#f5eaea';
+  Chart.defaults.font.family = 'Exo 2';
+  // Chart.defaults.global.defaultFontColor = '#f5eaea';
+  // Chart.defaults.global.defaultFontFamily = 'Exo 2';
   new Chart(ctx, {
     type: 'line',
     data: {
